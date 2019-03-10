f1d9ebac28d6568a34d4e8fe27cda1604540c289
Nikhil Dosapati <Nikhil.Dosapati@ril.com>
2019-03-10 08:55:28 +0530

    No products found! added in admin.js

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,6 +3,10 @@ const deleteProduct = (btn) => {
     const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
     const productElement = btn.closest('article');
     
+
+
+    
+    
     fetch('/admin/product/' + prodId, {
         method: 'DELETE',
         headers: {
@@ -13,6 +17,15 @@ const deleteProduct = (btn) => {
     }).then(data => {
         console.log(data);
         productElement.parentNode.removeChild(productElement);
+        let cardsLength = document.getElementsByClassName('card').length;
+        console.log('length:',cardsLength);
+        if(cardsLength === 0){
+            let para = document.createElement("H1");
+            let node = document.createTextNode("No products Found!");
+            para.appendChild(node);
+            var element = document.getElementById("main");
+            element.appendChild(para);
+        }
     }).catch(err => {
         console.log(err);
     });
